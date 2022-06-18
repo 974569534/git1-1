4891b8394083ed0f1e2355256465f61d2c722109
974569534 <974569534@qq.com>
2022-06-18 16:24:11 +0800

    Initial commit

---
+++ src/main/java/cn/tedu/git11/Git11Application.java
@@ -0,0 +1,13 @@
+package cn.tedu.git11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Git11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Git11Application.class, args);
+    }
+
+}
---
+++ src/test/java/cn/tedu/git11/Git11ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tedu.git11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Git11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
